d3ee7f99ba5c9993bcf9622c761d3e9036812649
a.ricop <ricoweins10@gmail.com>
2015-05-19 18:30:24 +0200

    Validacion de entradas

--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,14 @@
-// Definicion del modelo de Quiz
+// Definicion del modelo de Quiz con validacion
 
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('Quiz',
-		{ pregunta: DataTypes.STRING,
-		  repuesta: DataTypes.STRING,
-		});
+		{ pregunta: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: {msg: " Falta Pregunta"}}
+		},
+		repuesta: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: {msg: " Falta Respuesta"}}
+		}
+	});
 }
